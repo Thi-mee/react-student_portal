ab10a5159cbc4efe7c5b9709505d0c8d9e526770
Timi Adenuga <timswaggs1@outlook.com>
2022-11-18 20:49:51 +0100

    changed sidebar.css into a css module

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
 import dp from '../assets/profilepics.JPG'
-import '../css/sidebar.css'
+import style from '../css/sidebar.module.css'
 
 const SideBar = props => {
   
   return (
-    <aside className="side__bar">
-      <div className="side__bar__image">
+    <aside className={style.side__bar}>
+      <div className={style.side__bar__image}>
         <img src={dp} alt="profile" />
       </div>
       <h3>{props.name}</h3>
       
-      <div className="side__bar__links">
-        <Link to="/" className="side__bar__link">  DashBoard  </Link>
-        <Link to="profile" className="side__bar__link">  Profile  </Link>
-        <Link to="account" className="side__bar__link">  Account  </Link>
-        <Link to="courses" className="side__bar__link">  Courses  </Link>
-        <Link to="concepts" className="side__bar__link">  Concepts  </Link>
+      <div className={style.side__bar__links}>
+        <Link to="/" className={style.side__bar__link}>  DashBoard  </Link>
+        <Link to="profile" className={style.side__bar__link}>  Profile  </Link>
+        <Link to="account" className={style.side__bar__link}>  Account  </Link>
+        <Link to="courses" className={style.side__bar__link}>  Courses  </Link>
+        <Link to="concepts" className={style.side__bar__link}>  Concepts  </Link>
         <Link to="community" className="side__bar__link">  Community  </Link>
       </div>
     </aside>
